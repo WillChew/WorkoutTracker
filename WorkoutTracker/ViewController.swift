633192837771e497f11eac826b72378549242d43
WillChew <williamchew95@gmail.com>
2020-01-22 02:49:00 -0500

    Initial Commit

---
+++ WorkoutTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkoutTracker
+//
+//  Created by Will Chew on 2020-01-22.
+//  Copyright © 2020 Will Chew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
